4acfb04efc870b7c1ba7b5ee41c8dfb9e1a1f17d
David <raymondinf23@gmail.com>
2020-11-17 00:02:35 -0400

    modal interface created

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,29 @@
 const logo = document.querySelector(".logo");
+const cardButton = document.querySelectorAll(".card-button");
+const modal = document.querySelector("#modal");
+const modalTitle = document.querySelector("#title");
 
 logo.addEventListener("click", () => {
-
 
     window.location.href = "/public/index.html"
 
 });
+
+cardButton.forEach(button => {
+
+    button.addEventListener("click", e => {
+        e.preventDefault();
+        
+        modal.style.display = "flex";
+
+    })
+})
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+
+    modalTitle.value = "";
+
+  }
